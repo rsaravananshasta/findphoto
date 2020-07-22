b5e69e5f4a5ee2887e4cd94f9b6b1762ff12052f
Saravanan Rajagopal <rsaravanan@shastatek.com>
2020-07-22 12:31:36 +0530

    before formik

--- src/components/SubmitPhoto/SubmitPhoto.js
+++ src/components/SubmitPhoto/SubmitPhoto.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import { Button,Container,Card,Col,Form} from 'react-bootstrap';
 import  './SubmitPhoto.css';
+import PropTypes from 'prop-types';
 // import Alert from 'react-s-alert';
 // import { reduxForm, Field } from 'redux-form';
 
@@ -8,7 +9,7 @@ class SubmitPhoto extends Component {
 
     constructor(props){
         super(props);
-        this.state = {value: ''};
+        this.state = {validated: ''};
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -22,12 +23,62 @@ class SubmitPhoto extends Component {
 
    
     handleChange(event) {
-        this.setState({value: event.target.value});
+        console.log(event.target);  
+    }
+
+    validateImage(event){
+        let allowedFileTypes = ['image/png','image/jpeg'];
+        let fileType = event.target.files[0].type;
+        let fileSize = event.target.files[0].size / 1024 / 1024; // in MB
+        if (fileSize > 2) {
+            alert('File size exceeds 2 MB');
+            event.target.classList.add('is-invalid');
+        }else if(allowedFileTypes.indexOf(fileType) == -1){
+            alert('Invalid File Type');
+            event.target.classList.add('is-invalid');
+        }else{
+            event.target.classList.remove('is-invalid');
         }
+
+
+    }
     
     handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.value);
-    event.preventDefault();
+        
+        const form = event.currentTarget;
+        //form.bootstrapValidator('enableFieldValidators', 'formGridDOB',false)
+        if (form.checkValidity() === false) {
+            console.log("inside");
+            event.target.className += " was-validated";
+            event.preventDefault();
+            event.stopPropagation();
+            return false;
+            
+        }
+
+        let postdata = {};
+        let data =  event.target;
+        postdata.childPhoto = data.formGridChildPhoto.files[0];
+        postdata.currentPhoto = data.formGridCurrentPhoto.files[0];
+        postdata.name = data.formGridName.value;
+        postdata.jobTitle = data.formGridJobTitle.value;
+        postdata.department = data.formGridDepartment.value;
+        postdata.email = data.formGridEmail.value;
+        postdata.jobLocation = data.formGridJobLocation.value;
+        postdata.gender = data.formGridGender.value;
+        postdata.teamName = data.formGridTeamName.value;
+        postdata.dob = data.formGridDOB.value;
+        postdata.doj = data.formGridDOJ.value;
+        console.log(postdata);
+        
+ 
+      
+        //const data = event.target.form.elements.formGridName.value;
+        // data.name = form.elements.formGridName.value
+        // data.jobtitle = form.elements.formGridJobTitle.value
+        alert('Form submitted: ' + postdata.toString());
+        event.preventDefault();
+       // event.target.reset();
     }
 
     render(){
@@ -36,78 +87,88 @@ class SubmitPhoto extends Component {
             <div className="container register">
                 <div className="row">
                     <div className="col-md-3 register-left">
-                        <img src="https://image.ibb.co/n7oTvU/logo_white.png" alt=""/>
+                        <img src="https://www.lowes.com/images/logos/2016_lowes_logo/lowes_logo_rgb/LLowesLogo2016_Vertical_RGB.png" alt=""/>
                         <h3>Welcome</h3>
                         <p>Please submit your details to join</p>
                         
                     </div>
                     <div className="col-md-9 register-right">
-                        <Form>
-                            <div  id="home" role="tabpanel" aria-labelledby="home-tab">
+                        <Form noValidate  onSubmit={this.handleSubmit}>
+                            <div  id="home">
                                 <h3 className="register-heading">Submit your Entries</h3>
                                 <div className="row register-form">
                                     <div className="col-md-6">
-                                        <div className="form-group">
-                                            <label>Childhood Photo</label>
-                                            <input type="file" className="form-control" placeholder="First Name *" value="" />
-                                        </div>
-                                        <div className="form-group">
-                                            <label>Current Photo</label>
-                                            <input type="file" className="form-control" placeholder="First Name *" value="" />
-                                        </div>
-                                        <div className="form-group">
-                                            <label>Name</label>
-                                            <input type="text" className="form-control" placeholder="Name *" value="" />
-                                        </div>
-                                        <div className="form-group">
-                                            <label>Job Title</label>
-                                            <input type="text" className="form-control"  placeholder="Job Title *" value="" />
-                                        </div>
-                                        <div className="form-group">
-                                            <label>DOB</label>
-                                            <input type="date" className="form-control" placeholder="DOB *" value="" />
-                                        </div>
-                                        <div className="form-group">
-                                            <label>Gender</label>
-                                            <div className="maxl">
-                                                <label className="radio inline"> 
-                                                    <input type="radio" name="gender" value="male" checked />
-                                                    <span> Male </span> 
-                                                </label>
-
-                                                <label className="radio inline"> 
-                                                    <input type="radio" name="gender" value="female" />
-                                                    <span>Female </span> 
-                                                </label>
-                                            </div>
-                                        </div>
+                                        <Form.Group controlId="formGridChildPhoto">
+                                            <Form.Label>Childhood Photo *</Form.Label>
+                                            <Form.Control required  className="form-control"  type="file" accept="image/*" onChange={this.validateImage}/>
+                                            <Form.Text className="text-muted">
+                                                Max file size 2 MB, Dimension 200 X 200
+                                            </Form.Text>
+                                            {/* <Form.Control.Feedback type="invalid">
+                                                Maximum allowed file size exceeded.
+                                            </Form.Control.Feedback> */}
+                                        </Form.Group>
+                                        <Form.Group controlId="formGridCurrentPhoto">
+                                            <Form.Label>Current Photo *</Form.Label>
+                                            <Form.Control required className="form-control"  type="file" className="form-control" accept="image/*" onChange={this.validateImage} />
+                                            <Form.Text className="text-muted">
+                                                Max file size 2 MB, Dimension 200 X 200
+                                            </Form.Text>
+                                        </Form.Group>
+                                        <Form.Group controlId="formGridName">
+                                            <Form.Label>Name *</Form.Label>
+                                            <Form.Control required  className="form-control" placeholder="Name *" onchange={this.handleChange} />
+                                        </Form.Group>
+                                        <Form.Group controlId="formGridJobTitle">
+                                            <Form.Label>Job Title *</Form.Label>
+                                            <Form.Control required  className="form-control"  placeholder="Job Title *"  onchange={this.handleChange}  />
+                                        </Form.Group>
+                                        <Form.Group controlId="formGridDOB">
+                                            <Form.Label>DOB</Form.Label>
+                                            <Form.Control  type="date" className="form-control " placeholder="DOB *"  onchange={this.handleChange}   />
+                                        </Form.Group>
+                                        <Form.Group controlId="formGridGender">
+                                            <Form.Label>Gender</Form.Label>
+                                            <Form.Row  as={Col} >
+                                                <Form.Check inline type="radio" checked value="male" label="Male" name="formGridGender"  onchange={this.handleChange}  />
+                                                <Form.Check inline type="radio" value="female" label="Female" name="formGridGender"  onchange={this.handleChange} />
+                                            </Form.Row>
+                                        </Form.Group>
                                     </div>
                                     <div className="col-md-6">
-                                        <div className="form-group">
-                                            <label>Department</label>
-                                            <input type="text" className="form-control" placeholder="Department *" value="" />
-                                        </div>
-                                        <div className="form-group">
-                                            <label>Lowes email id</label>
-                                            <input type="email" className="form-control" placeholder="Lowes email id *" value="" />
-                                        </div>
-                                        <div className="form-group">
-                                            <label>Team Name</label>
-                                            <input type="text" className="form-control" placeholder="Team Name *" value="" />
-                                        </div>
-                                        <div className="form-group">
-                                            <label>Job Location</label>
-                                            <select className="form-control">
-                                                <option className="hidden"  selected disabled>Select</option>
+                                        <Form.Group controlId="formGridDepartment">
+                                            <Form.Label>Department *</Form.Label>
+                                            <Form.Control required  className="form-control" placeholder="Department *" onchange={this.handleChange}  />
+                                            <Form.Text className="text-muted">
+                                                &nbsp;
+                                            </Form.Text>
+                                        </Form.Group>
+                                        <Form.Group controlId="formGridEmail">
+                                            <Form.Label>Lowes Email id *</Form.Label>
+                                            <Form.Control required  type="email" className="form-control" placeholder="Lowes Email id *" onchange={this.handleChange}/>
+                                            <Form.Text className="text-muted">
+                                                We'll never share your email with anyone else.
+                                            </Form.Text>
+                                        </Form.Group>
+                                        <Form.Group controlId="formGridTeamName">
+                                            <Form.Label>Team Name *</Form.Label>
+                                            <Form.Control required  className="form-control" placeholder="Team Name *"  onchange={this.handleChange} />
+                                        </Form.Group>
+                                        <Form.Group controlId="formGridJobLocation">
+                                            <Form.Label>Job Location *</Form.Label>
+                                            <Form.Control required  as="select"   defaultValue="Choose..." className="form-control"  onchange={this.handleChange}>
                                                 <option>India</option>
                                                 <option>United States</option>
-                                            </select>
-                                        </div>
-                                        <div className="form-group">
-                                            <label>DOJ</label>
-                                            <input type="date"  className="form-control" placeholder="" value="" />
-                                        </div>
-                                        <input type="submit" className="btnRegister"  value="Submit"/>
+                                            </Form.Control>
+                                        </Form.Group>
+                                        <Form.Group required="false" controlId="formGridDOJ">
+                                            <Form.Label>DOJ</Form.Label>
+                                            <Form.Control type="date"   className="form-control" placeholder=""  onchange={this.handleChange} />
+                                        </Form.Group>
+                                        <Form.Group required="false" controlId="formGridDOJ">
+                                        <Button type="submit" required="false" className="btnRegister"  label="Submit">Submit</Button>
+                                        </Form.Group>
+                                        
                                     </div>
                                 </div>
                             </div>
@@ -116,107 +177,6 @@ class SubmitPhoto extends Component {
                 </div>
 
             </div>
-            
-        )
-    }
-
-
-    render_(){
-    
-        return(
-            <Card>
-                    <Container className="p-3">
-                    <Form>
-                        <Form.Row>
-                        <Form.Group as={Col} controlId="formGridEmail">
-                            <Form.Label>Childhood photo</Form.Label>
-                            <Form.Control type="file" placeholder="Childhood photo" />
-                        </Form.Group>
-                    
-                        <Form.Group as={Col} controlId="formGridPassword">
-                            <Form.Label>Current photo</Form.Label>
-                            <Form.Control type="file" placeholder="Current photo" />
-                        </Form.Group>
-                        </Form.Row>
-                
-                
-                        <Form.Row>
-                        <Form.Group  as={Col}   controlId="formGridAddress1">
-                        <Form.Label>Name</Form.Label>
-                        <Form.Control placeholder="Name" />
-                        </Form.Group>
-                        <Form.Group as={Col}  controlId="formGridAddress2">
-                        <Form.Label>Job title</Form.Label>
-                        <Form.Control placeholder="Apartment, studio, or floor" />
-                        </Form.Group>
-                    
-                        </Form.Row>
-                        <Form.Row>
-                    
-                        <Form.Group  as={Col}   controlId="formGridAddress2">
-                        <Form.Label>Department</Form.Label>
-                        <Form.Control placeholder="Apartment, studio, or floor" />
-                        </Form.Group>
-                        <Form.Group    as={Col}    controlId="formGridCity">
-                            <Form.Label>Team Name</Form.Label>
-                            <Form.Control />
-                        </Form.Group>
-                    
-                        </Form.Row>
-                    
-                    
-                        <Form.Row>
-                            <Form.Group controlId="formGridAddress2">
-                            <Form.Label>Lowes email id</Form.Label>
-                            <Form.Control placeholder="Apartment, studio, or floor" />
-                            </Form.Group>
-                        </Form.Row>
-                        
-                        <Form.Row>
-                            <Form.Group  controlId="formGridState">
-                                <Form.Label>Job location</Form.Label>
-                                <Form.Control as="select" defaultValue="Choose...">
-                                <option>India</option>
-                                <option>US</option>
-                                </Form.Control>
-                            </Form.Group> 
-                        </Form.Row>
-
-                        <Form.Row>
-                            <Form.Group  controlId="formGridState">
-                                <Form.Label>DOB</Form.Label>
-                                <Form.Control />
-                            </Form.Group> 
-                        </Form.Row>
-
-                        <Form.Row>
-                            <Form.Group  controlId="formGridState">
-                                <Form.Label>DOJ</Form.Label>
-                                <Form.Control />
-                            </Form.Group> 
-                        </Form.Row>
-
-                        <Form.Row>
-                            <Form.Group  controlId="formGridState">
-                                <Form.Label>Gender</Form.Label>
-                                <Form.Control as="select" defaultValue="Choose...">
-                                <option>Male</option>
-                                <option>Female</option>
-                                </Form.Control>
-                            </Form.Group> 
-                        </Form.Row>
-                    
-
-                        <Form.Group id="formGridCheckbox">
-                        <Form.Check type="checkbox" label="Check me out" />
-                        </Form.Group>
-                
-                        <Button variant="primary" type="submit">
-                        Submit
-                        </Button>
-                    </Form>
-                </Container>
-        </Card>
             
         )
     }
